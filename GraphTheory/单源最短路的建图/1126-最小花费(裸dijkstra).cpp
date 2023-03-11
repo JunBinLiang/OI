1f31bf4982515d9e2ad7aeaef193f7ae1444daa4
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-03-10 22:51:05 -0500

    Update and rename 1126-最小花费.cpp to 1126-最小花费(裸dijkstra).cpp

--- GraphTheory/单源最短路的建图/1126-最小花费(裸dijkstra).cpp
+++ GraphTheory/单源最短路的建图/1126-最小花费(裸dijkstra).cpp
@@ -85,3 +85,58 @@ int main() {
     }
     return 0;
 }
+
+
+/*
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 2010;
+
+int n, m, S, T;
+double g[N][N];
+double dist[N];
+bool st[N];
+
+void dijkstra()
+{
+    dist[S] = 1;
+    for (int i = 1; i <= n; i ++ )
+    {
+        int t = -1;
+        for (int j = 1; j <= n; j ++ )
+            if (!st[j] && (t == -1 || dist[t] < dist[j]))
+                t = j;
+        st[t] = true;
+
+        for (int j = 1; j <= n; j ++ )
+            dist[j] = max(dist[j], dist[t] * g[t][j]);
+    }
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+
+    while (m -- )
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        double z = (100.0 - c) / 100;
+        g[a][b] = g[b][a] = max(g[a][b], z);
+    }
+
+    cin >> S >> T;
+
+    dijkstra();
+
+    printf("%.8lf\n", 100 / dist[T]);
+
+    return 0;
+}
+
+*/
